6bf05295c66f4c69898743b65048a3e9243173b9
learnworksmart <learnworksmart@gmail.com>
2022-10-11 08:58:20 +0800

    read nessus csv output

--- scan.py
+++ scan.py
@@ -5,20 +5,35 @@ from dotenv import load_dotenv
 def main(): 
   print("Hello world!")
   nessus_ouput_csv = './samples/Nessus-Scan.csv'
-  affected_services = extractAffectedServices(nessus_ouput_csv)
-  print(affected_services)
+  print(getAffectedServices(nessus_ouput_csv))
 
-def extractAffectedServices(input):
-  affected_services = []
-  with open(input) as csvfile:
+def getAffectedServices(input):
+  unique_services = getUniqueServices(input)
+  return checkTls(unique_services, input)
+
+def checkTls(services, input):
+  for i in range(len(services)): 
+    host, port, cipher = services[i].split(':')
+    with open(input) as csvfile:
       reader = csv.DictReader(csvfile)
       for row in reader:
-        # service in 127.0.0.1:3128
-        service = "{}:{}".format(row['Host'],row['Port'])
-        # avoid adding duplicate
-        if service not in affected_services:    
-          affected_services.append(service)
-  return affected_services
+        if row['Host'] == host and row['Port'] == port and cipher == 'no':
+          # TLSv1.2 is enabled and the server supports at least one cipher.'
+          if 'cipher' in row['Plugin Output']:
+            services[i] = services[i].replace('no', 'yes')
+  return services
+
+def getUniqueServices(input):
+  unique_services = []
+  with open(input) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      # service in 127.0.0.1:3128:no
+      service = "{}:{}:no".format(row['Host'],row['Port'])
+      # avoid adding duplicate
+      if service not in unique_services:    
+        unique_services.append(service)
+  return unique_services
 
 # execute run time
 main()
